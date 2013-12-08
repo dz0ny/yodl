9918c7f2bf2e6664406be454ad598242faf45e60
Janez Troha <dz0ny@users.noreply.github.com>
2013-12-08 22:08:42 +0100

    Update app.js

--- yodl/static/app.js
+++ yodl/static/app.js
@@ -1,34 +1,3 @@
-;(function(window, Raven, console) {
-'use strict';
-
-var originalConsole = console,
-    logLevels = ['debug', 'info', 'warn', 'error'],
-    level;
-
-var logForGivenLevel = function(level) {
-    return function () {
-        var args = [].slice.call(arguments);
-        Raven.captureMessage('' + args, {level: level, logger: 'console'});
-
-        // this fails for some browsers. :(
-        if (originalConsole[level]) {
-             originalConsole[level].apply(null, args);
-        }
-    };
-};
-
-
-level = logLevels.pop();
-while(level) {
-    console[level] = logForGivenLevel(level);
-    level = logLevels.pop();
-}
-// export
-window.console = console;
-
-}(this, Raven, console || {}));
-
-
 angular.module('yomm', [
     'controllers'
 ])
